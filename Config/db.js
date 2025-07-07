44077c791dc5bf9a4499d705f78e5a6eda217f08
Mounesh v <mouneshv696@gmail.com>
2025-07-07 22:19:00 +0530

    db.js

--- Config/db.js
+++ Config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://127.0.0.1:27017/elearningDB");
+    const conn = await mongoose.connect("mongodb+srv://mouneshv696:fK1FgmZEAgZdYMGb@cluster0.bqmn9xz.mongodb.net/");
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
